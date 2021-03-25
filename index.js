752b52464f0323192db0bcc74bf5c5d2c0247efa
Marco-PachecoG <lokyfactormiedo@gmail.com>
2021-03-25 17:38:01 -0600

    examen 4

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const db = firebase.firestore();
+
 const taskForm = document.getElementById('task-form');
 
-taskForm.addEventListener('submit', e => {
+const saveTask = (nombre, direccion, telefono, email) =>
+    db.collection("tasks").doc().set({
+        nombre,
+        direccion,
+        telefono,
+        email,
+    });
+
+taskForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const title
+    const nombre = taskForm["task-name"];
+    const direccion = taskForm["task-direccion"];
+    const telefono = taskForm["task-telefono"];
+    const email = taskForm["task-email"];
+    
+    await saveTask(nombre.value, direccion.value, telefono.value, email.value);
+
+    taskForm.reset();
+    nombre.focus();
+
+    console.log(nombre, direccion, telefono, direccion);
 });
